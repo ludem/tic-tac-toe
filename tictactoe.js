61b1ca8a59a2f9eb51cf85e4201284cbededdf3b
ludem <luca.demil@gmail.com>
2017-08-09 16:32:44 +0200

    style improved; project completed

--- tictactoe.js
+++ tictactoe.js
@@ -1,52 +1,74 @@
+//global variables
 let gameStatus = "running";
 let playerSymbol = "X";
 let computerSymbol = "O";
 
+//html elements
 const cells = document.querySelectorAll(".cell");
 const symbols = document.querySelectorAll(".symbol");
+const result = document.querySelector(".result");
 
 //add listeners
 symbols.forEach(x => x.addEventListener("click", symbolChoice));
 cells.forEach(x => x.addEventListener("click", playerTurn));
 
-//check game status
-const isGameRunning = () => gameStatus === 'running';
+//function to check game status
+const isGameRunning = () => gameStatus === "running";
 
+//assign 'x' or 'o' according to player's choice
 function symbolChoice() {
   playerSymbol = this.textContent;
-  computerSymbol = playerSymbol === 'X' ? 'O' : 'X';
-  resetGame(); 
+  computerSymbol = playerSymbol === "X" ? "O" : "X";
+
+  //change style
+  symbols.forEach(x => x.classList.remove("active"));
+  this.classList.add("active");
+  resetGame();
 }
 
+//handle player turn
 function playerTurn() {
+  //if game is not running, reset the game and continue
   if (!isGameRunning()) resetGame();
   //check if the cell is empty
   if (this.textContent !== "") return;
   this.textContent = playerSymbol;
   if (checkWin(Array.from(cells), playerSymbol)) {
     gameStatus = "Player wins";
+    result.textContent = gameStatus;
     return;
   }
-  computerTurn();
+  //brief timeout before computer move
+  setTimeout(computerTurn, 200);
 }
 
 function computerTurn() {
   //select only empty cells
   const freeCells = Array.from(cells).filter(x => !x.textContent);
+
   //check if there are still cells available
   if (freeCells.length == 0) return;
-  const randomFreeCell = pickRandomCell(freeCells);
-  randomFreeCell.textContent = computerSymbol;
+
+  //chose a cell among the available
+  const chosenCell = pickRandomCell(freeCells);
+
+  chosenCell.textContent = computerSymbol;
+
+  //check game status
   if (checkWin(Array.from(cells), computerSymbol)) {
     gameStatus = "Computer wins";
+    result.textContent = gameStatus;
     return;
   }
 }
 
+/*Computer only choose a random cell
+AI to be improved*/
 function pickRandomCell(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+//check if there's a winner
 function checkWin(arr, symbol) {
   const table = arr.map(x => x.textContent);
   return checkRows() || checkColumns() || checkDiags();
@@ -75,9 +97,9 @@ function checkWin(arr, symbol) {
   }
 }
 
+//reset game
 function resetGame() {
-  cells.forEach(x => x.textContent = '');
-  gameStatus = 'running';
+  cells.forEach(x => (x.textContent = ""));
+  gameStatus = "running";
+  result.textContent = '';
 }
-
-
